75e0200290df6ce2019baa516904bd187a35c7fd
Oliver <hoklaichau@gmail.com>
2020-07-30 21:03:27 +0800

    Add new file

---
+++ 123.py
@@ -0,0 +1,20 @@
+x = 5
+print(x)
+y = 10
+print(y)
+z = (x + y)
+print(z)
+price = 10
+pen = 0.38
+rain = True
+name = 'Oliver'
+print(price, pen, rain, name)
+
+name = input('who are you?')
+print('HI', name)
+
+height = input('what is your height?')
+print(height)
+
+weight = input('what is your weight')
+print(weight)
